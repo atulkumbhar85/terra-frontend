4951bc842d93e2a27b3f4d006d90a2f3d6de472a
atulkumbhar85 <atul.kumbhar1985@gmail.com>
2025-12-16 00:38:51 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const FLASK_API_URL = process.env.FLASK_API_URL || 'http://localhost:5000';
+// const FLASK_API_URL = 'http://127.0.0.1:5000';
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'form.html'));
+});
+
+app.post('/submit-form', async (req, res) => {
+    try {
+        const todo = req.body;
+        const response = await axios.post(`${FLASK_API_URL}/add`, { 
+            task: todo.task,
+            description: todo.description
+         });
+        console.log("Flask backend response", response.data);
+
+        res.json({
+            success: true,
+            data: response.data,
+            message: 'Form submitted successfully'
+        });
+    } catch (error) {
+        console.error('Error communicating with Flask API:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Frontend server is running on http://localhost:${PORT}`);
+    console.log(`Flask API is running on ${FLASK_API_URL}`);
+});
